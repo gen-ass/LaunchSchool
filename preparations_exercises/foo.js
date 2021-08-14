c286a8dcc19754f9eac82a53a1124c2ebdc91e18
John Smith <johnsmith444@example.com>
2021-08-14 11:46:09 +0200

    basics done

--- preparations_exercises/foo.js
+++ preparations_exercises/foo.js
@@ -75,4 +75,97 @@ true                // boolean literal
 [ 1, 2, 3 ]         // array literal
 undefined           // undefined literal
 
+> '1' + '2' 
+this is only string concatenation, beacuase the quotation marks are part of the JavaScript syntax: '12' // a string"
+
+> typeof'1' - string + typeof(2) - number 
+'12' this is now one string
+This will concatenate the string with the number and make both a string value, we call this implicit type coercion. It forces the number to become a string.
+
+> Number('1')
+= 1
+
+In this example we implicitly coerced the string to be a number. We used the Number() function to do so.
+
+> Number('foo')
+= NaN
+
+This will raise an error in other languages but not in JavaScript it fails silently.
+
+> parseInt('12')
+= 12
+
+The parseInt() function will aslo coerce strings to numbers. The added benefit it has, is that it will read the numbers in a string and then ingnore the other charachters.
+
+Example:
+> parseInt('12xyz')
+= 12
+
+But it will ignore the any string number value that has fractions in it.
+
+> parseInt('3.1415')
+= 3
+
+If the number is to big it will show up with Infinity or -Infinity values
+
+
+We also have the parseFloat() function which will coerce a string with  to a float with numerical values
+
+> parseFloat('12.5foo')
+= 12.5
+
+https://launchschool.com/books/javascript/read/basics#datastructures
+Data Structures
+
+Arrays
+Arrays are ordered list of values any data type can be placed in an array. Elelment Indexes are non-negative and atarts at 0. 
+[1, 2, 3, 4, 5]
+
+> [1, 2, 3, 4, 5][0] // starts at Zero
+= 1
+
+Important aspect about arrays:
+
+- The order of the elements is significant.
+- Use index numbers to retrieve array elements.
+- Index numbers are non-negative integers starting from 0.
+
+Objects
+It is a dictionary like structure that uses keys to identify certain values
+
+> { dog: 'barks' } // the key is a string:dog, the value:'barks' can be any data type
+= { dog: 'barks' }
+
+> ({ dog: 'barks', cat: 'meows', pig: 'oinks' })['cat']
+= 'meows' // to access the value we use the key ['cat'] which prints 'meows'()
+
+
+https://launchschool.com/books/javascript/read/basics#expressionsandreturnvalues
+Expresions and Return vlaues
+
+Almost every value you type in JS gets evaluated
+
+We call this return value
+
+> console.log(5 + 2)
+7
+= undefined // each line is evaluated, in this case the console.log() method also gets evaluated, and this is undefined.
+
+output and return values are no the same
+
+Statements and Expresions
+
+>let foo = 3; 
+You can't call/re-assign the statement let - it's value cannot be captured and reused later in your code. The expresion on the right = 3 can be altered or called
+
+'By this definition, every chunk of code that can be treated as a single unit is a statement. This includes:
+
+- variable, function, and class declarations
+- loops and if statements
+- return and break statements
+- assignments: a = 3;
+- standalone expressions: console.log("Hello");'
+
+Not all expressions are part of all statements.
+
 */
---
+++ preparations_exercises/intro-exercise.js
@@ -0,0 +1,106 @@
+/* intro-excercise
+
+1.Concatenate two or more strings, one with your first name and one with your last, to create a string with your full name as its value. For example, if your name is John Doe, think about how you can put 'John' and 'Doe' together to get 'John Doe'.
+> 'Peter ' + 'Parker' or console.log('Peter' + 'Parker');
+= 'Peter Parker'
+
+2. Use the arithmetic operators to determine the individual digits of a 4-digit number like 4936:
+    thousands place is 4
+    hundreds place is 9
+    tens place is 3
+    ones place is 6
+
+> let number = 4936
+> let ones = number % 10
+> ones
+= 6
+
+> number = (number - ones) / 10
+= 493
+
+> let tens = number % 10
+> tens
+= 3
+
+> number = (number - tens) / 10
+= 49
+
+> let hundreds = number % 10
+> hundreds
+= 9
+
+> let thousands = (number - hundreds) / 10
+> thousands
+= 4
+
+3 Identify the data type for each of the following values:
+
+    'true' = string
+    false = bolean
+    1.5 = number
+    2 =  number
+    undefined = undefined
+    { foo: 'bar' } = object
+
+4 Explain why this code logs '510' instead of 15.
+
+console.log('5' + 10); // The first value console logged of '5' is a stirng the second value console logged is a number. When a string and a number is "added" together, JS will implicitly coerce the number to a string, the printed value is a string. Every + operation will be coerced to a string.
+
+5 Refactor the code from the previous exercise to use explicit coercion, so it logs 15 instead.
+
+> console.log(Number('5') + 10);
+= 15
+> parseInt('5') + 10 or console.log(parseInt('5') + 10);
+= 15
+> parseFloat('5') + 10 or console.log(parseFloat('5') + 10);
+= 15
+
+
+6 Use the template literal syntax along with the expression Number('5') + 10 to log the following sentence to the console:
+
+The value of 5 + 10 is 15.
+> `The value of 5 + 10 is ${Number('5') + 10}.` or console.log(`The value of 5 + 10 is ${Number('5') + 10}.`);
+
+7 Will an error occur if you try to access an array element with an index that is greater than or equal to the length of the array? For example:
+
+
+let foo = ['a', 'b', 'c'];
+console.log(foo.length);  // => 3
+console.log(foo[3]);      // will this result in an error? It will result in a undefined value. The Array only has 3 elements in the array. The request is for a fourth element on index 3, the index count in the current array is 2 and there is only three elements.
+
+8 Create an array named; names that contains a list of pet names. For instance:
+
+names = [
+'pasta',
+'butterscotch',
+'pudding',
+'neptune',
+'darwin',
+]
+
+9 Create an object named pets that contains a list of pet names and the type of animal. For instance:
+
+let pets = {
+pasta: 	'dog',
+butterscotch: 	'cat',
+pudding: 	'cat',
+neptune: 	'fish',
+darwin: 	'lizard'
+}
+
+10 What value does the following expression evaluate to?
+
+> parseInt('3.1415')
+= 3 // parseInt() will convert the string to a number, but it will only convert the string before the decimal to a number, the rest will be ignored.
+
+12 What value does the following expression evaluate to?
+
+> '12' < '9'
+= true // JS does a character by character evalution on the string. This implies, '1' < '9' so '12' is less than it is true. If you add '92' < '9' it becomes false, as '9' is not less but the same.
+
+https://launchschool.com/books/javascript/read/variables#variablesandvariablenames
+
+Variables and Names
+
+
+*/
