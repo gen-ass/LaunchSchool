94fd60867bf721235803eaa2571a32b00c4d03a0
AlistairDev <25964915+gen-ass@users.noreply.github.com>
2021-08-14 08:34:05 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,82 @@
+console.log('Hello, world!');
+
+/*let answerToUltimateQuestion = 42; // initializing a variable
+let aRoundNumber = 3.141528;       // another comment
+
+
+/* This is a multi-line comment.
+   It can span any number of lines.
+   The command ends when you see
+   an * followed by a / as shown
+   here ==> */
+
+// You can also use /* and */ to embed comments in the middle of a line:
+// let area = w /* width */ * h /* height */;
+// You should avoid embedded comments
+
+/*if (isOk()){              // bad
+  // do something
+}
+
+if (isOk())               // bad
+{
+  // do something
+}
+
+if (isOk()) {             // good
+  // do something
+}
+
+
+function isOk(){          // bad
+  // do something
+}
+
+function isOk()           // bad
+{
+  // do something
+}
+
+function isOk() {         // good
+  // do something
+}
+
+
+let sum=x+5;              // bad
+let sum = x + 5;          // good
+
+
+let answerToUltimateQuestion = 42;     // initializing a variable
+function fourScoreAndSevenYearsAgo() { // defining a function
+  // do something
+}
+
+
+function DomesticCat(name) {           // defining a function, like constructor functions
+	  // do something
+}
+
+
+const INTEREST_RATE = 0.0525;
+const COURSE_NUMBER = 'JS101';
+const HOST = 'launchschool.com';
+const FIRST_LETTER = 'a';             // magic number, these are configuration values that will not change or be altered, scremaing snake case 
+const LAST_LETTER = 'z';              //
+
+
+const SECONDS_PER_MINUTE = 60;
+const OUNCES_PER_POUND = 16;
+const METERS_PER_KILOMETER = 1000;		// These are magic numbers that should not change or are scremaing snake case.
+const PI = 3.141528;
+const INPUT_PROMPT = '==>';
+const TODAY = new Date();
+
+// these are the camelCase for functions 
+const sayHi = function() {
+  console.log("Hi!");
+};
+
+// these are PascalCase for constructor funtions
+const Pet = function(name) {
+  this.name = name;
+};*/
