840e2871069e63d8bf09cb942cef4a103425e68f
AlistairDev <25964915+gen-ass@users.noreply.github.com>
2024-01-18 22:50:07 +0100

    Remove character from last part of string

---
+++ preparations_exercises/remove-character.js
@@ -0,0 +1,9 @@
+// Create a function removeLastChar that takes a string as an argument, and returns the string without the last character.
+
+
+function removeLastChar(string) {
+	return string.substring(0, string.length - 1);
+}
+
+removeLastChar('ciao!'); // 'ciao'
+removeLastChar('hello'); // 'hell'
