6d6fb528c9cdf77068fd081003dbf5355f3472c1
AlistairDev <25964915+gen-ass@users.noreply.github.com>
2024-01-18 23:08:29 +0100

    Added new version wirh edits

--- preparations_exercises/remove-character.js
+++ preparations_exercises/remove-character.js
@@ -5,5 +5,5 @@ function removeLastChar(string) {
 	return string.substring(0, string.length - 1);
 }
 
-removeLastChar('ciao!'); // 'ciao'
-removeLastChar('hello'); // 'hell'
+console.log(removeLastChar('ciao!')); // 'ciao'
+console.log(removeLastChar('hello')); // 'hell'
